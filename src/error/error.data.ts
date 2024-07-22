803ca722b8aed09970fffd50147838370f2a5877
Marek Kędzia <marekkedzia>
2024-07-22 21:40:55 +0200

    fix build error

--- src/error/error.data.ts
+++ src/error/error.data.ts
@@ -1,9 +1,7 @@
-import {config} from "../config";
-
 export enum ErrorData {
     INTERNAL_SERVER_ERROR = "Internal error, we are working on it",
     INVALID_BODY = "Invalid body, please check your request and try again",
     INVALID_QUERY = "Invalid query, please check your request and try again",
     FORBIDDEN = "You are not allowed to access this resource",
-    ID_PREFIX = `ID prefix must match ${config.idPrefixRegex.toString()}`
+    ID_PREFIX = "Invalid id prefix, please use the correct prefix"
 }
